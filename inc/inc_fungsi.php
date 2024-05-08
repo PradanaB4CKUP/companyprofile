c79af0f5d66bcc78082743230e35d7e5e5b36bce
PradanaB4CKUP <aji2207ok@gmail.com>
2024-05-08 22:41:56 +0700

    baru ubah koneksi

--- inc/inc_fungsi.php
+++ inc/inc_fungsi.php
@@ -2,7 +2,7 @@
 function url_dasar(){
     //$_SERVER['SERVER_NAME'] : alamat website, misalkan websitemu.com
     // $_SERVER['SCRIPT_NAME'] : directory website, websitemu.com/blog/ $_SERVER['SCRIPT_NAME'] : blog
-    $url_dasar  = "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']);
+    $url_dasar  = "http://".$_SERVER['localhost/'].dirname($_SERVER['website-company-profile/']);
     return $url_dasar;
 }
 function ambil_gambar($id_tulisan){
